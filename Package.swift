910589f735f3b3a2da9a70849f12841ba2733042
Kostiantyn Zakharov <kszakharov@gmail.com>
2026-05-10 23:50:39 -0400

    feat: add swift-argument-parser dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,21 @@ import PackageDescription
 let package = Package(
   name: "rada",
   platforms: [.macOS(.v26)],
-  targets: [
-    .executableTarget(name: "rada")
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-argument-parser",
+      from: "1.7.1"
+    )
   ],
+  targets: [
+    .executableTarget(
+      name: "rada",
+      dependencies: [
+        .product(
+          name: "ArgumentParser",
+          package: "swift-argument-parser"
+        )
+      ]
+    )
+  ]
 )
